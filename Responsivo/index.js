e0a2b1736d94d140916461bd749601d8d6a21fca
Miguel Muniz de Camargo <miguelmunizdecamargo322@gmail.com>
2026-05-17 12:55:49 -0300

    Add files via upload

--- Responsivo/index.js
+++ Responsivo/index.js
@@ -1,17 +1,19 @@
 function estilizarbotao(){
- 
-    let nomezinha = ""
-    let senhazinha = ""
+
+
+    let nomezinha = document.getElementById("bloco-1").value;
+    let senhazinha = document.getElementById("bloco").value;
  
     if(nomezinha == "Miguel Muniz" && senhazinha == "12345") {
         console.log("Usuário autentico!")
         alert("Usuário autentico!")
+
+         window.location.href = "home.html"; 
     }else{
-        console.log("Senha incorreta!")
-         alert("Senha incorreta!")
+        console.log("Senha incorreta! Tente novamente.")
+         alert("Senha incorreta! Tente novamente.")
+          
     }
 
 
 }
-
-
